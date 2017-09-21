24cee17ef66c46cf071cdb4de9477a99484a1180
TelmaA <telma.afonso94@gmail.com>
2017-09-21 17:01:35 +0100

    [ADD] New function to write reactions/metabolites list to file. Other changes.

--- yeastpack_test.py
+++ yeastpack_test.py
@@ -8,6 +8,7 @@ Author: Telma Afonso
 
 from cobra.io.sbml import create_cobra_model_from_sbml_file
 from yeastpack.io import load_yeast_76
+from yeastpack.simulation import fba, fva, pfba, lmoma, simulate_auxotrophy, simulate_essentiality
 from types import *
 import pickle
 
@@ -19,7 +20,7 @@ def checkModelInfo (model, n = 5):
         model (cobra.core.Model object) : model from cobra.Model
         n (integer) : determines amount of information shown
     '''
-    assert type(n) == int, "%r is not an integer" % n
+    assert isinstance(n, int), 'n must be an integer'
 
     print('Model ID: ', model.id)
     print('Number of reactions:', len(model.reactions))
@@ -27,10 +28,10 @@ def checkModelInfo (model, n = 5):
     print('Number of genes:', len(model.genes))
     try:
         print('First %i reactions:' % n)
-        [print('\t', x.id, x.reaction) for x in model.reactions[:n]]
+        [print('\t', x.id, x.name) for x in model.reactions[:n]]
     except IndexError:
         print('First %i reactions:' % len(model.reactions))
-        [print('\t', x.id, x.reaction) for x in model.reactions[:len(model.reactions)]]
+        [print('\t', x.id, x.name) for x in model.reactions[:len(model.reactions)]]
     try:
         print('First %i metabolites:' % n)
         [print('\t', x.id, x.name) for x in model.metabolites[:n]]
@@ -45,7 +46,8 @@ def checkModelInfo (model, n = 5):
         [print('\t', x.id, x.name, end = " ") for x in model.genes[:len(model.genes)]]
     print('Compartments:')
     [print('\t', ID, c) for ID, c in model.compartments.items()]
-
+    print('Yeastpack model description:')
+    [print('\t', key, value) for key, value in model.describe().items()]
 
 def saveModelToFile (model, filename):
     ''' Saves a model object to a file for later use
@@ -55,7 +57,7 @@ def saveModelToFile (model, filename):
         model (cobra.core.Model object) : model from cobra.Model
         filename (str) : the name of the file to store the model
     '''
-    assert type(filename) == str, "%r is not a string" % filename
+    assert isinstance(filename, str), 'filename must be a string'
 
     with open(filename, 'wb') as f:
         pickle.dump(model, f)
@@ -71,12 +73,34 @@ def loadModelFromFile (filename):
         ----------
         model (cobra.core.Model object) : model from cobra.Model
     '''
-    assert type(filename) == str, "%r is not a string" % filename
+    assert isinstance(filename, str), 'filename must be a string'
 
     with open(filename, 'rb') as f:
         model = pickle.load(f)
 
     return model
+
+
+def writeMetReactToFile (filename, type = 'metabolites'):
+    ''' Writes the list of metabolites/reactions to a file
+
+    Parameters
+    ----------
+    filename (str) : the name of the file to be created
+    type (str): whether to write the reactions or metabolites to the file
+    '''
+    assert isinstance(filename, str), 'filename must be a string'
+    assert type in ['metabolites', 'reactions'], "type must be either 'metabolites' or 'reactions'"
+
+    dict = {}
+    if type == 'metabolites':
+        [dict.update({x.id: x.name}) for x in model.metabolites[:len(model.metabolites)]]
+    else:
+        [dict.update({x.id: x.name}) for x in model.reactions[:len(model.reactions)]]
+
+    with open(filename, 'w') as f:
+        for key, val in dict.items():
+            f.write(key + '\t' + val + '\n')
 
 
 
@@ -95,6 +119,37 @@ if __name__ == '__main__':
 
     # Load the model from file
     model = loadModelFromFile('model_yeast_76.sav')
+    model.summary()
+
+    model.metabolites.s_1203.summary() #examine the overall redox balance of the model
+    model.metabolites.s_0434.summary() #main energy production and consumption reactions
+
+    #SIMULATIONS
+    #FBA
+    res_fba = fba(model)
+    res_fba.objective_value     # The (optimal) value for the objective function.
+    res_fba.status              # The solver status related to the solution.
+    res_fba.fluxes              # Contains the reaction fluxes (primal values of variables).
+    res_fba.reduced_costs       # Contains reaction reduced costs (dual values of variables).
+    res_fba.shadow_prices       # Contains metabolite shadow prices (dual values of constraints).
+
+    #PFBA
+    res_pfba = pfba(model)
+    res_pfba.f                  # The objective value
+    res_pfba.solver             # A string indicating which solver package was used.
+    res_pfba.x                  # List or Array of the fluxes (primal values).
+    res_pfba.x_dict             # A dictionary of reaction IDs that maps to the respective primal values.
+    res_pfba.y                  # List or Array of the dual values.
+    res_pfba.y_dict             # A dictionary of reaction IDs that maps to the respective dual values.
+
+    #FVA
+    res_fva = fva(model, reaction_list = model.reactions) #pandas dataframe
+    res_fva.to_csv('fva_res.csv', sep = '\t')
+
+    
+    writeMetReactToFile('reactions.csv', type = 'reactions')
+
+
 
 
 
