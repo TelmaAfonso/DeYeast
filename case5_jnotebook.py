4635f34205aed334391f0b84a86b4bdaa636ec68
TelmaAfonso <telma.afonso94@gmail.com>
2017-12-14 16:14:28 +0000

    Added scripts for case 5.

---
+++ case5_jnotebook.py
@@ -0,0 +1,134 @@
+
+'''
+Jupyter Notebook for Case 5
+
+Author: Telma Afonso
+'''
+
+import nbformat as nbf
+
+nb = nbf.v4.new_notebook()
+
+text = """\
+# Case 5 Report
+This report contains the results with case 5 simulations.
+"""
+
+code = """\
+import warnings
+from case_5 import *
+
+pd.set_option('display.max_colwidth', -1)
+warnings.filterwarnings('ignore')
+
+#Initialization
+case5 = Case5()
+case5.model = case5.loadObjectFromFile('model_yeast_76.sav')
+case5.model.solver = 'optlang-cplex'
+case5.setMedium('MINIMAL')
+case5.dictsForCase5()
+
+"""
+
+datasets_text = """\
+## General datasets
+"""
+
+datasets_code = """\
+#General datasets
+exp_dataset, reactions = case5.loadExperimentalRes('Results/Case 5/case5_experimental_fluxes.csv')
+pd.DataFrame(reactions)
+"""
+
+fba_text = """\
+## Flux Balance Analysis (FBA) Simulation
+"""
+
+pfba_text = """\
+## Parsimonious Flux Balance Analysis (pFBA) Simulation
+"""
+
+fva_text = """\
+## Flux Variability Analysis (FVA) Simulation
+"""
+
+
+# ===== GLUCOSE =====
+
+g_text = """\
+# Glucose carbon source
+"""
+
+g_fba_datasets = """\
+g_exp_df = case5.getColumnWithoutNAs(exp_dataset, 0)
+# O2 flux estimation not possible (ethanol flux of 0 independently of O2 flux)
+
+g_fba_res, g_fba_exp_sim, g_fba_exp_sim_errors = case5.simulationPipeline(g_exp_df, cs = 'glucose', type = 'fba', res_exists = True, fname = 'Results/Case 5/res_fba_glucose_case5.sav')
+pd.concat([reactions, g_fba_exp_sim_errors], axis = 1, join = 'inner') #Plot not showing r_0302
+"""
+
+g_pfba_datasets = """\
+g_pfba_res, g_pfba_exp_sim, g_pfba_exp_sim_errors = case5.simulationPipeline(g_exp_df, cs = 'glucose', type = 'pfba', res_exists = True, fname = 'Results/Case 5/res_pfba_glucose_case5.sav')
+pd.concat([reactions, g_pfba_exp_sim_errors], axis = 1, join = 'inner')
+"""
+
+g_fva_datasets = """\
+g_fva_res, g_fva_exp_sim, _ = case5.simulationPipeline(g_exp_df, cs = 'glucose', type = 'fva', res_exists = True, fname = 'Results/Case 5/res_fva_glucose_case5.sav')
+pd.concat([reactions, g_fva_exp_sim], axis = 1, join = 'inner')
+"""
+
+
+# ===== ETHANOL =====
+
+e_text = """\
+# Ethanol carbon source
+"""
+
+e_fba_datasets = """\
+e_exp_df = case5.getColumnWithoutNAs(exp_dataset, 1)
+
+e_fba_res, e_fba_exp_sim, e_fba_exp_sim_errors = case5.simulationPipeline(e_exp_df, cs = 'ethanol', type = 'fba', res_exists = False, fname = 'Results/Case 5/res_fba_ethanol_case5.sav')
+pd.concat([reactions, e_fba_exp_sim_errors], axis = 1, join = 'inner') #Plot not showing r_0302
+"""
+
+e_pfba_datasets = """\
+e_pfba_res, e_pfba_exp_sim, e_pfba_exp_sim_errors = case5.simulationPipeline(e_exp_df, cs = 'ethanol', type = 'pfba', res_exists = False, fname = 'Results/Case 5/res_pfba_ethanol_case5.sav')
+pd.concat([reactions, e_pfba_exp_sim_errors], axis = 1, join = 'inner')
+"""
+
+e_fva_datasets = """\
+e_fva_res, e_fva_exp_sim, _ = case5.simulationPipeline(e_exp_df, cs = 'ethanol', type = 'fva', res_exists = False, fname = 'Results/Case 5/res_fva_ethanol_case5.sav')
+pd.concat([reactions, e_fva_exp_sim], axis = 1, join = 'inner')
+"""
+
+#Generate cells with plots
+x = sum([['g' + i, 'e' + i] for i in ['_fba', '_pfba']], [])
+for name in x:
+    vars()[name + '_plot'] = '<p style="float: center; font-size: 9pt; text-align: center; width: 80%;"><img src = "Results/Case 5/'+ name +'_exp_sim_plot.png", width = 100%></p>'
+
+#List with nbformat expressions
+cs = ['g', 'e']
+nbcells = [['nbf.v4.new_markdown_cell(' + s + '_text)',
+            'nbf.v4.new_markdown_cell(fba_text)',
+            'nbf.v4.new_code_cell(' + s + '_fba_datasets)',
+            'nbf.v4.new_markdown_cell(' + s + '_fba_plot)',
+            'nbf.v4.new_markdown_cell(pfba_text)',
+            'nbf.v4.new_code_cell(' + s + '_pfba_datasets)',
+            'nbf.v4.new_markdown_cell(' + s + '_pfba_plot)',
+            'nbf.v4.new_markdown_cell(fva_text)',
+            'nbf.v4.new_code_cell(' + s + '_fva_datasets)'] for s in cs]
+
+nbcells = [item for sublist in nbcells for item in sublist]
+
+
+nb['cells'] = [nbf.v4.new_markdown_cell(text),
+               nbf.v4.new_code_cell(code),
+               nbf.v4.new_markdown_cell(datasets_text),
+               nbf.v4.new_code_cell(datasets_code)] + [eval(exp) for exp in nbcells]
+
+
+
+with open('case5.ipynb', 'w') as f:
+    nbf.write(nb, f)
+
+
---
+++ case_5.py
@@ -0,0 +1,225 @@
+
+'''
+Functions for case 5 simulations
+
+Author: Telma Afonso
+'''
+
+from phenomenaly.simulation import fba, fva, pfba, lmoma
+from phenomenaly.variables import Media
+from types import *
+import pickle
+import string
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+from matplotlib.backends.backend_pdf import PdfPages
+
+import warnings
+warnings.filterwarnings('ignore')
+pd.set_option('display.max_colwidth', -1)
+
+from yeastpack_test import PhenomenalySim
+
+
+class Case5 (PhenomenalySim):
+
+    def __int__(self, cobra_model = None):
+        super(Case5, self).__int__(self, cobra_model)
+
+    def dictsForCase5 (self):
+        # Carbon source lbs
+        cs_lb = {'glucose': -1.5, 'ethanol': -5} #Shophia's values
+
+        # Carbon source exchange reactions
+        cs_reaction = {'glucose': 'r_1714', 'ethanol': 'r_1761'}
+
+        self.cs_lb, self.cs_reaction = cs_lb, cs_reaction
+
+    def loadExperimentalRes (self, filename_exp, sep = ';'):
+        exp_fluxes = pd.read_csv(filename_exp, sep = sep)
+        exp_fluxes.set_index('Yeast7_ID', inplace = True)
+        reactions = exp_fluxes.iloc[:,0]
+        sub_exp_fluxes = exp_fluxes.drop([col for col in list(exp_fluxes.columns) if 'exp flux' not in col], 1)
+        sub_exp_fluxes = sub_exp_fluxes.apply(lambda x: x.str.replace(',', '.'))
+        #sub_exp_fluxes = sub_exp_fluxes.apply(lambda x: x.astype('float') if str(x).isdigit() else None)
+
+        #Add Biomass Values (r_4041)
+        # reactions['r_4041'] = 'Biomass'
+        # sub_exp_fluxes.loc['r_4041'] = [0.33, 0.32, 0.20, 0.10] #growth rate
+
+        return sub_exp_fluxes, reactions
+
+    def getColumnWithoutNAs (self, dataframe, column_index, na = 'x'):
+        df = dataframe[dataframe.ix[:, column_index] != na].ix[:, column_index]
+
+        return df.astype('float')
+
+    def getDFWithoutExtremeFluxes (self, dataframe, column_index = 1, val = 900):
+        df = dataframe[(dataframe.ix[:, column_index] > -val) & (dataframe.ix[:, column_index] < val)]
+
+        return df.astype('float')
+
+    def createDatasetExpVsSimul (self, exp_fluxes, sim_fluxes):
+        dsim = sim_fluxes.copy()
+        df = pd.concat([exp_fluxes, sim_fluxes], axis = 1, join = 'inner')
+
+        return df
+
+    def createDatasetWithAbsRelError (self, dataset_exp_vs_sim):
+        df = dataset_exp_vs_sim.copy()
+        ae = self.absoluteError(df.ix[:, 0], df.ix[:, 1])
+        df.insert(loc = 2, column = 'Abs Error', value = ae)
+
+        re = self.relativeError(df.ix[:, 0], df.ix[:, 1])
+        df.insert(loc = 3, column = 'Rel Error', value = re)
+
+        return df
+
+    def simulationPipeline (self, exp_dataset, cs = 'glucose', o2_lb = None, geneko = None, type = 'fba', res_exists = False, fname = None):
+        if res_exists:
+            res = self.loadObjectFromFile(fname)
+        else:
+            res = self.singleSimulation(carbon_source = self.cs_reaction[cs], cs_lb = self.cs_lb[cs], geneko = geneko, o2_lb = o2_lb, type = type)
+            self.saveObjectToFile(res, fname)
+
+        if type == 'fva':
+            fluxes = res #pandas df
+        elif hasattr(res, 'x_dict'): #if legacy solution
+            fluxes = pd.DataFrame(list(res.x_dict.items())).set_index(0)
+        else:
+            fluxes = res.fluxes
+
+        # Dataset with experimental vs simulated fluxes
+        df_exp_sim = self.createDatasetExpVsSimul(exp_dataset, self.correctReversedReactions(fluxes))
+        if type != 'fva':
+            df_exp_sim = df_exp_sim.rename(columns = {1: 'Sim Flux'})
+
+        # Dataset with absolute and realtive errors
+        df_exp_sim_errors = self.createDatasetWithAbsRelError(df_exp_sim)
+
+        return res, df_exp_sim, df_exp_sim_errors
+
+    def plotExpVsSim (self, absRelErrorDataset, xlab = 'Experimental Flux', ylab = 'Simulated Flux', title = 'Wild Type', label_adjust = 0.05, save_fig_path = None):
+        plt.rcParams["figure.figsize"] = (10,5)
+
+        x = absRelErrorDataset.ix[:,0]
+        y = absRelErrorDataset.ix[:,1]
+        react_IDs = list(absRelErrorDataset.index)
+        slope, intercept, r_value, p_value, std_err = linregress(x, y)
+        line = [slope * x + intercept for x in x]
+        meanRelErr = absRelErrorDataset.ix[:,3].mean()
+        corr = x.corr(y)
+
+        plt.plot(x, y, 'o', x, line)
+        for ind, react_ID in enumerate(react_IDs):
+            plt.annotate(react_ID, (x[ind], y[ind]), fontsize = 8, xytext = (x[ind] + label_adjust, y[ind] + label_adjust))
+
+        plt.ylabel(ylab)
+        plt.xlabel(xlab)
+        plt.title(title)
+        plt.plot([], [], ' ') # To show correlation in legend
+        plt.plot([], [], ' ') # To show mean relative error in legend
+        plt.legend(['Reactions', 'R2: %.4f' % r_value**2, 'Pearson correlation: %.4f' % corr, 'Mean relative error: %.4f' % meanRelErr])
+
+        if save_fig_path is not None:
+            plt.savefig(save_fig_path)
+
+    def testO2EthanolProd (self, g_knockout = None, cs = 'r_1714', cs_lb = -1.5, range_o2 = list(np.arange(-20, 0, 2))):
+        loading_bars = 40*'='
+        res = {}
+        for i in range_o2:
+            with self.model as m:
+                m.set_carbon_source(cs, lb = cs_lb)
+                m.reactions.get_by_id('r_1992').lower_bound = float(i)
+                if g_knockout is not None:
+                    m.set_environmental_conditions(gene_knockout = g_knockout)
+                r = pfba(m)
+                if hasattr(r, 'x_dict'): #if legacy solution
+                    fluxes = pd.DataFrame(list(r.x_dict.items())).set_index(0)
+                else:
+                    fluxes = r.fluxes
+                res[str(i)] = fluxes.loc['r_0163']
+        for key, val in sorted(res.items()): print(key, '\t', val)
+        return res
+
+    def plotO2vsEtOH (self, dict_EtOH_res, real_EtOH_flux = 0, xlab = 'O2 Flux', ylab = 'EtOH Flux', title = 'Ethanol production with O2 flux', legend = 'Wild Type', fname = None):
+        plt.figure(figsize = (10, 5))
+        x = sorted([float(x) for x in dict_EtOH_res.keys()])
+        y = [float(dict_EtOH_res[str(key)]) for key in x]
+        slope, intercept, r_value, p_value, std_err = linregress(x, y)
+        line = [slope * x + intercept for x in x]
+        real_O2 = lambda x0: (y0 - intercept) / slope
+        y0 = real_EtOH_flux
+        plt.plot(x, y, 'o', x, line)
+        plt.axhline(y = real_EtOH_flux, ls = 'dashed')
+        plt.xlabel(xlab)
+        plt.ylabel(ylab)
+        plt.title(title)
+        plt.legend([legend, 'R2: %.4f' % r_value**2, 'Real EtOH flux: %.2f (O2 flux of %.2f)' % (real_EtOH_flux, real_O2(y0))])
+        plt.show()
+
+        if fname is not None:
+            plt.savefig(fname)
+
+        return round(real_O2(y0), 4)
+
+
+#r_0163
+
+#if __name__ == '__main__':
+
+#Strain FY4 is derived from S288C (very similar)
+
+#Initialization
+case5 = Case5()
+case5.model = case5.loadObjectFromFile('model_yeast_76.sav')
+case5.model.solver = 'optlang-cplex'
+case5.setMedium('MINIMAL')
+case5.dictsForCase5()
+
+#General datasets
+exp_dataset, reactions = case5.loadExperimentalRes('Results/Case 5/case5_experimental_fluxes.csv')
+
+# ====== CS: GLUCOSE ======
+g_exp_df = case5.getColumnWithoutNAs(exp_dataset, 0)
+
+# O2 FLUX ESTIMATION - ALL ZERO!
+# g_etOH = case5.testO2EthanolProd(range_o2 = list(np.arange(-10, 0, 1)))
+# case5.saveObjectToFile(g_etOH, 'Results/Case 5/g_dict_etOH_O2_fluxes.sav')
+# g_etOH = case5.loadObjectFromFile('Results/Case 5/g_dict_etOH_O2_fluxes.sav')
+# g_o2_lb = case5.plotO2vsEtOH(g_etOH, real_EtOH_flux = -0.2478, fname = 'Results/Case 5/g_etOH_plot.png')
+# plt.close('all')
+
+#FBA
+g_fba_res, g_fba_exp_sim, g_fba_exp_sim_errors = case5.simulationPipeline(g_exp_df, cs = 'glucose', type = 'fba', res_exists = True, fname = 'Results/Case 5/res_fba_glucose_case5.sav')
+g_fba_exp_sim_errors = case5.getDFWithoutExtremeFluxes(g_fba_exp_sim_errors) #without r_0302, for plotting
+case5.plotExpVsSim(g_fba_exp_sim_errors, save_fig_path = 'Results/Case 5/g_fba_exp_sim_plot.png', title = 'FBA Glucose Carbon Source')
+plt.close('all')
+
+#pFBA
+g_pfba_res, g_pfba_exp_sim, g_pfba_exp_sim_errors = case5.simulationPipeline(g_exp_df, cs = 'glucose', type = 'pfba', res_exists = True, fname = 'Results/Case 5/res_pfba_glucose_case5.sav')
+case5.plotExpVsSim(g_pfba_exp_sim_errors, save_fig_path = 'Results/Case 5/g_pfba_exp_sim_plot.png', title = 'pFBA Glucose Carbon Source')
+plt.close('all')
+
+#FVA
+g_fva_res, g_fva_exp_sim, _ = case5.simulationPipeline(g_exp_df, cs = 'glucose', type = 'fva', res_exists = True, fname = 'Results/Case 5/res_fva_glucose_case5.sav')
+
+
+# ====== CS: ETHANOL ======
+e_exp_df = case5.getColumnWithoutNAs(exp_dataset, 1)
+
+#FBA
+e_fba_res, e_fba_exp_sim, e_fba_exp_sim_errors = case5.simulationPipeline(e_exp_df, cs = 'ethanol', type = 'fba', res_exists = False, fname = 'Results/Case 5/res_fba_ethanol_case5.sav')
+e_fba_exp_sim_errors = case5.getDFWithoutExtremeFluxes(e_fba_exp_sim_errors) #without r_0302, for plotting
+case5.plotExpVsSim(e_fba_exp_sim_errors, save_fig_path = 'Results/Case 5/e_fba_exp_sim_plot.png', title = 'FBA Ethanol Carbon Source')
+plt.close('all')
+
+#pFBA
+e_pfba_res, e_pfba_exp_sim, e_pfba_exp_sim_errors = case5.simulationPipeline(e_exp_df, cs = 'ethanol', type = 'pfba', res_exists = False, fname = 'Results/Case 5/res_pfba_ethanol_case5.sav')
+case5.plotExpVsSim(e_pfba_exp_sim_errors, save_fig_path = 'Results/Case 5/e_pfba_exp_sim_plot.png', title = 'pFBA Ethanol Carbon Source')
+plt.close('all')
+
+#FVA
+e_fva_res, e_fva_exp_sim, _ = case5.simulationPipeline(e_exp_df, cs = 'ethanol', type = 'fva', res_exists = False, fname = 'Results/Case 5/res_fva_ethanol_case5.sav')
