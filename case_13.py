642b8deea52f56534aeb4e03b2895ed2b5c74c62
TelmaAfonso <telma.afonso94@gmail.com>
2018-01-10 16:46:41 +0000

    Updated case 13 results.

--- case_13.py
+++ case_13.py
@@ -127,6 +127,48 @@ class Case13 (PhenomenalySim):
         if save_fig_path is not None:
             plt.savefig(save_fig_path)
 
+    def testO2EthanolProd (self, g_knockout = None, react_id = 'r_2115', cs = 'g_oxidative', range_o2 = list(np.arange(-20, 0, 2))):
+        res = {}
+        for i in range_o2:
+            with self.model as m:
+                m.set_carbon_source(self.cs_reaction[cs], lb = self.cs_lb[cs])
+                m.reactions.get_by_id('r_1992').lower_bound = float(i)
+                if g_knockout is not None:
+                    m.set_environmental_conditions(gene_knockout = g_knockout)
+                r = pfba(m)
+                if hasattr(r, 'x_dict'): #if legacy solution
+                    fluxes = pd.DataFrame(list(r.x_dict.items())).set_index(0)
+                else:
+                    fluxes = r.fluxes
+                res[str(i)] = fluxes.loc[react_id]
+        for key, val in sorted(res.items()): print(key, '\t', val)
+        return res
+
+    def plotO2vsEtOH (self, dict_EtOH_res, real_EtOH_flux = 0, xlab = 'O2 Flux', ylab = 'EtOH Flux', title = 'Ethanol production with O2 flux', legend = 'Wild Type', fname = None):
+        plt.figure(figsize = (10, 5))
+        try:
+            x = sorted([float(x) for x in dict_EtOH_res.keys()])
+            y = [float(dict_EtOH_res[str(key)]) for key in x]
+        except:
+            x = sorted([int(x) for x in dict_EtOH_res.keys()])
+            y = [int(dict_EtOH_res[str(key)]) for key in x]
+        slope, intercept, r_value, p_value, std_err = linregress(x, y)
+        line = [slope * x + intercept for x in x]
+        real_O2 = lambda x0: (y0 - intercept) / slope
+        y0 = real_EtOH_flux
+        plt.plot(x, y, 'o', x, line)
+        plt.axhline(y = real_EtOH_flux, ls = 'dashed')
+        plt.xlabel(xlab)
+        plt.ylabel(ylab)
+        plt.title(title)
+        plt.legend([legend, 'R2: %.4f' % r_value**2, 'Real EtOH flux: %.2f (O2 flux of %.2f)' % (real_EtOH_flux, real_O2(y0))])
+        plt.show()
+
+        if fname is not None:
+            plt.savefig(fname)
+
+        return round(real_O2(y0), 4)
+
 
 #if __name__ == '__main__':
 
@@ -143,6 +185,13 @@ exp_dataset, reactions = case13.loadExperimentalRes('Results/Case 13/case13_expe
 # ====== OXIDATIVE GROWTH ======
 o_exp_df = case13.getColumnWithoutNAs(exp_dataset, 0, 'X')
 # NO EtOH fluxes available for O2 flux estimation
+
+# O2 FLUX ESTIMATION - EtOH production with O2 flux above -2.60
+# o_etOH = case13.testO2EthanolProd(cs = 'g_oxidative', range_o2 = list(np.arange(-3, -1, 0.1)))
+# case13.saveObjectToFile(o_etOH, 'Results/Case 13/o_dict_etOH_O2_fluxes.sav')
+# o_etOH = case13.loadObjectFromFile('Results/Case 13/o_dict_etOH_O2_fluxes.sav')
+# o_o2_lb = case13.plotO2vsEtOH(o_etOH, real_EtOH_flux = 0.004, legend = 'Oxidative Growth', fname = 'Results/Case 13/o_etOH_plot.png')
+# plt.close('all')
 
 #FBA
 o_fba_res, o_fba_exp_sim, o_fba_exp_sim_errors = case13.simulationPipeline(o_exp_df, cs = 'g_oxidative', type = 'fba', res_exists = True, fname = 'Results/Case 13/res_fba_oxidative_case13.sav')
@@ -163,37 +212,51 @@ o_fva_res, o_fva_exp_sim, _ = case13.simulationPipeline(o_exp_df, cs = 'g_oxidat
 rf_exp_df = case13.getColumnWithoutNAs(exp_dataset, 1, 'X')
 # NO EtOH fluxes available for O2 flux estimation
 
+# O2 FLUX ESTIMATION - O2 flux of -5.57
+# rf_etOH = case13.testO2EthanolProd(cs = 'g_resp_fermentative', range_o2 = list(np.arange(-6, -3, 0.2)))
+# case13.saveObjectToFile(rf_etOH, 'Results/Case 13/rf_dict_etOH_O2_fluxes.sav')
+# rf_etOH = case13.loadObjectFromFile('Results/Case 13/rf_dict_etOH_O2_fluxes.sav')
+# rf_o2_lb = case13.plotO2vsEtOH(rf_etOH, real_EtOH_flux = 2.43, legend = 'Respiro-Fermentative Growth', fname = 'Results/Case 13/rf_etOH_plot.png')
+# plt.close('all')
+
 #FBA
-rf_fba_res, rf_fba_exp_sim, rf_fba_exp_sim_errors = case13.simulationPipeline(rf_exp_df, cs = 'g_resp_fermentative', type = 'fba', res_exists = True, fname = 'Results/Case 13/res_fba_resp_fermentative_case13.sav')
+rf_fba_res, rf_fba_exp_sim, rf_fba_exp_sim_errors = case13.simulationPipeline(rf_exp_df, cs = 'g_resp_fermentative', o2_lb = -5.57, type = 'fba', res_exists = True, fname = 'Results/Case 13/res_fba_resp_fermentative_case13.sav')
 rf_fba_exp_sim_errors = case13.getDFWithoutExtremeFluxes(rf_fba_exp_sim_errors) #without extreme fluxes (for plotting)
 case13.plotExpVsSim(rf_fba_exp_sim_errors, save_fig_path = 'Results/Case 13/rf_fba_exp_sim_plot.png', title = 'FBA Respiro-Fermentative Growth')
 plt.close('all')
 
 #pFBA
-rf_pfba_res, rf_pfba_exp_sim, rf_pfba_exp_sim_errors = case13.simulationPipeline(rf_exp_df, cs = 'g_resp_fermentative',type = 'pfba', res_exists = True, fname = 'Results/Case 13/res_pfba_resp_fermentative_case13.sav')
+rf_pfba_res, rf_pfba_exp_sim, rf_pfba_exp_sim_errors = case13.simulationPipeline(rf_exp_df, o2_lb = -5.57, cs = 'g_resp_fermentative',type = 'pfba', res_exists = True, fname = 'Results/Case 13/res_pfba_resp_fermentative_case13.sav')
 case13.plotExpVsSim(rf_pfba_exp_sim_errors, save_fig_path = 'Results/Case 13/rf_pfba_exp_sim_plot.png', title = 'pFBA Respiro-Fermentative Growth')
 plt.close('all')
 
 #FVA
-rf_fva_res, rf_fva_exp_sim, _ = case13.simulationPipeline(rf_exp_df, cs = 'g_resp_fermentative', type = 'fva', res_exists = True, fname = 'Results/Case 13/res_fva_resp_fermentative_case13.sav')
+rf_fva_res, rf_fva_exp_sim, _ = case13.simulationPipeline(rf_exp_df, cs = 'g_resp_fermentative', o2_lb = -5.57, type = 'fva', res_exists = True, fname = 'Results/Case 13/res_fva_resp_fermentative_case13.sav')
 
 
 # ====== FERMENTATIVE GROWTH ======
 f_exp_df = case13.getColumnWithoutNAs(exp_dataset, 2, 'X')
 # NO EtOH fluxes available for O2 flux estimation
 
+# O2 FLUX ESTIMATION - O2 flux of
+# f_etOH = case13.testO2EthanolProd(cs = 'g_fermentative', range_o2 = list(np.arange(-6, -3, 0.2)))
+# case13.saveObjectToFile(f_etOH, 'Results/Case 13/f_dict_etOH_O2_fluxes.sav')
+# f_etOH = case13.loadObjectFromFile('Results/Case 13/f_dict_etOH_O2_fluxes.sav')
+# f_o2_lb = case13.plotO2vsEtOH(f_etOH, real_EtOH_flux = 8.84, legend = 'Fermentative Growth', fname = 'Results/Case 13/f_etOH_plot.png')
+# plt.close('all')
+
 #FBA
-f_fba_res, f_fba_exp_sim, f_fba_exp_sim_errors = case13.simulationPipeline(f_exp_df, cs = 'g_fermentative', type = 'fba', res_exists = True, fname = 'Results/Case 13/res_fba_fermentative_case13.sav')
+f_fba_res, f_fba_exp_sim, f_fba_exp_sim_errors = case13.simulationPipeline(f_exp_df, cs = 'g_fermentative', o2_lb = -4.91, type = 'fba', res_exists = True, fname = 'Results/Case 13/res_fba_fermentative_case13.sav')
 f_fba_exp_sim_errors = case13.getDFWithoutExtremeFluxes(f_fba_exp_sim_errors) #without extreme fluxes (for plotting)
 case13.plotExpVsSim(f_fba_exp_sim_errors, save_fig_path = 'Results/Case 13/f_fba_exp_sim_plot.png', title = 'FBA Fermentative Growth')
 plt.close('all')
 
 #pFBA
-f_pfba_res, f_pfba_exp_sim, f_pfba_exp_sim_errors = case13.simulationPipeline(f_exp_df, cs = 'g_fermentative',type = 'pfba', res_exists = True, fname = 'Results/Case 13/res_pfba_fermentative_case13.sav')
+f_pfba_res, f_pfba_exp_sim, f_pfba_exp_sim_errors = case13.simulationPipeline(f_exp_df, cs = 'g_fermentative', o2_lb = -4.91,type = 'pfba', res_exists = True, fname = 'Results/Case 13/res_pfba_fermentative_case13.sav')
 case13.plotExpVsSim(f_pfba_exp_sim_errors, save_fig_path = 'Results/Case 13/f_pfba_exp_sim_plot.png', title = 'pFBA Fermentative Growth')
 plt.close('all')
 
 #FVA
-f_fva_res, f_fva_exp_sim, _ = case13.simulationPipeline(f_exp_df, cs = 'g_fermentative', type = 'fva', res_exists = True, fname = 'Results/Case 13/res_fva_fermentative_case13.sav')
+f_fva_res, f_fva_exp_sim, _ = case13.simulationPipeline(f_exp_df, cs = 'g_fermentative', o2_lb = -4.91, type = 'fva', res_exists = True, fname = 'Results/Case 13/res_fva_fermentative_case13.sav')
 
 
